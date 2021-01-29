2c517ddf2b4a31c64e0b656d4e546d81813c78d8
Felix Castro <felixgonzalocastro@gmail.com>
2021-01-28 23:13:21 -0500

    algoritmo markov lesto

--- components/markov.js
+++ components/markov.js
@@ -14,12 +14,31 @@ const matrizTransicionXestado = (matrizTransicion = [], estado = []) => {
   return newEstado
 }
 
+const restarEstados = (estadoFinal = [], estadoAnterior = []) => {
+  let newEstadoResta = []
+   for (let i = 0; i < estadoFinal.length; i++) {
+    newEstadoResta.push(estadoFinal[i]-estadoAnterior[i])
+  }
+  return newEstadoResta
+}
+
+const validarTope2 = (estadoResta = [], tope = 0.0001) => {
+  let newEstadoValidado = []
+  for (let i = 0; i < estadoResta.length; i++) {
+    if (estadoResta[i] < tope) {
+      newEstadoValidado.push(false)
+    } else {
+      newEstadoValidado.push(true)
+    }
+  }
+  return newEstadoValidado
+}
+
 const validarTope = (estadoFinal = [], estadoAnterior = [], tope = 0.0001) => {
   let newEstadoValidado = []
   let estadoTemp = []
   for (let i = 0; i < estadoFinal.length; i++) {
     estadoTemp.push(estadoFinal[i]-estadoAnterior[i])
-    
   }
   for (let i = 0; i < estadoTemp.length; i++) {
     if (estadoTemp[i] < tope) {
@@ -31,12 +50,12 @@ const validarTope = (estadoFinal = [], estadoAnterior = [], tope = 0.0001) => {
   return newEstadoValidado
 }
 
-const detenerAlgoritmo = (estadoValidados = []) => {
-  if (estadoValidados.length === 0 ) {
+const detenerAlgoritmo = (UltimoeEstadoValidado = []) => {
+  if (UltimoeEstadoValidado.length === 0 ) {
     return true
   } else {
-    for (let i = 0; i < estadoValidados.length; i++) {
-      if (estadoValidados[i] === true) {
+    for (let i = 0; i < UltimoeEstadoValidado.length; i++) {
+      if (UltimoeEstadoValidado[i] === true) {
           return true;
       }
     }
@@ -44,43 +63,58 @@ const detenerAlgoritmo = (estadoValidados = []) => {
   }
 }
 
-// const objeto = {
-//   etiquetas: ['Movistar','Entel','Claro'],
-//   tope: 0.000001,
-//   matrizTransicion: [
-//     [0.6,0.3,0.3],
-//     [0.2,0.5,0.3],
-//     [0.2,0.2,0.4]
-//   ],
-//   estados:[
-//     [0.4,0.25,0.35]
-//   ],
-//   estadosValidados: []
-// }
-
 const objeto = {
   etiquetas: ['Movistar','Entel','Claro'],
   tope: 0.000001,
   matrizTransicion: [
-    [0.0,0.75,1],
-    [0.5,0.0,0],
-    [0.5,0.25,0]
+    [0.6,0.3,0.3],
+    [0.2,0.5,0.3],
+    [0.2,0.2,0.4]
   ],
   estados:[
-    [0.4,0.5,0.1]
+    [0.4,0.25,0.35]
   ],
+  estadosRestas: [],
   estadosValidados: []
 }
 
+// const objeto = {
+//   etiquetas: ['Movistar','Entel','Claro'],
+//   tope: 0.000001,
+//   matrizTransicion: [
+//     [0.0,0.75,1],
+//     [0.5,0.0,0],
+//     [0.5,0.25,0]
+//   ],
+//   estados:[
+//     [0.4,0.5,0.1]
+//   ],
+//   estadosRestas: [],
+//   estadosValidados: []
+// }
+
 // algoritmo
+// let cont = 0
+// objeto.estadosValidados.push(validarTope(objeto.estados[objeto.estados.length - 1], objeto.estados[objeto.estados.length - 2], objeto.tope)) 
+// while (detenerAlgoritmo(objeto.estadosValidados[objeto.estadosValidados.length - 1]) && cont < 100) {
+//   objeto.estados.push(matrizTransicionXestado(objeto.matrizTransicion, objeto.estados[objeto.estados.length - 1]))
+//   objeto.estadosValidados.push(validarTope(objeto.estados[objeto.estados.length - 1],objeto.estados[objeto.estados.length - 2], objeto.tope)) 
+//   cont = cont + 1
+// }
+
+// console.log(objeto)
+
+
 let cont = 0
-objeto.estadosValidados.push(validarTope(objeto.estados[objeto.estados.length - 1], objeto.estados[objeto.estados.length - 2], objeto.tope)) 
+
+objeto.estadosRestas.push(restarEstados(objeto.estados[objeto.estados.length - 1], objeto.estados[objeto.estados.length - 2]))
+objeto.estadosValidados.push(validarTope2(objeto.estadosRestas[objeto.estadosRestas.length - 1], objeto.tope))
+
 while (detenerAlgoritmo(objeto.estadosValidados[objeto.estadosValidados.length - 1]) && cont < 100) {
   objeto.estados.push(matrizTransicionXestado(objeto.matrizTransicion, objeto.estados[objeto.estados.length - 1]))
-  objeto.estadosValidados.push(validarTope(objeto.estados[objeto.estados.length - 1],objeto.estados[objeto.estados.length - 2], objeto.tope)) 
+  objeto.estadosRestas.push(restarEstados(objeto.estados[objeto.estados.length - 1], objeto.estados[objeto.estados.length - 2]))
+  objeto.estadosValidados.push(validarTope2(objeto.estadosRestas[objeto.estadosRestas.length - 1], objeto.tope))
   cont = cont + 1
 }
-
-
 
 console.log(objeto)
